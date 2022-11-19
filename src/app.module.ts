8d5ff51bdbf847a4ec89cc4d1866a23857c88faf
Amine Bouaziz <amine.bouaziz@concircle.com>
2022-11-19 18:05:16 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+      playground: true,
+    }),
+    DatabaseModule,
+  ],
+})
+export class AppModule {}
